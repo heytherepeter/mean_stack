e0e713bd5f46f86a061cbd8828436afdc36a9a95
Peter ONeill <jaretep@gmail.com>
2019-05-14 11:15:14 -0700

    playing with mongoose

--- Mongoose/mongoose.md
+++ Mongoose/mongoose.md
@@ -10,3 +10,48 @@ npm install body-parser --save
 npm install mongoose --save
 
 ```
+Connect to mongoose
+```javascript
+var mongoose = require('mongoose');
+
+// This is how we connect to the mongodb database using mongoose -- "basic_mongoose" is the name of
+//   our db in mongodb -- this should match the name of the db you are going to use for your project.
+mongoose.connect('mongodb://localhost/basic_mongoose');
+
+var UserSchema = new mongoose.Schema({
+    name: String,
+    age: Number
+   })
+   mongoose.model('User', UserSchema); // We are setting this Schema in our Models as 'User'
+   var User = mongoose.model('User') // We are retrieving this Schema from our Models, named 'User'
+   
+// Use native promises (only necessary with mongoose versions <= 4)
+mongoose.Promise = global.Promise;
+```
+
+Routes
+```javascript
+// Routes
+// Root Request
+app.get('/', function(req, res) {
+  User.find({}, function(err, users) {
+    if(err){res.render('index', {error: err});} 
+    else {res.render('index', {users: users});}
+    // This is where we will retrieve the users from the database and include them in the view page we will be rendering.
+  })
+});
+// Add User Request 
+app.post('/users', function(req, res) {
+    console.log("POST DATA", req.body);
+    // create a new User with the name and age corresponding to those from req.body
+    var user = new User({name: req.body.formName, age: req.body.formAge});
+    // Try to save that new user to the database (this is the method that actually inserts into the db) and run a callback function with an error (if any) from the operation.copy
+    user.save(function(err) {
+      // if there is an error console.log that something went wrong!
+      if(err) {console.log('something went wrong');
+      } else {console.log('successfully added a user!');
+          // else console.log that we did well and then redirect to the root route
+      }res.redirect('/');
+    })
+})
+```
--- Mongoose/server.js
+++ Mongoose/server.js
@@ -1,6 +1,11 @@
 // Require the Express Module
 var express = require('express');
 
+// add flash messages
+const flash = require('express-flash');
+
+const session = require('express-session');
+
 // require mongoose
 var mongoose = require('mongoose');
 
@@ -9,12 +14,14 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/basic_mongoose');
 
 var UserSchema = new mongoose.Schema({
-    name: String,
-    age: Number
-   })
-   mongoose.model('User', UserSchema); // We are setting this Schema in our Models as 'User'
-   var User = mongoose.model('User') // We are retrieving this Schema from our Models, named 'User'
-   
+  first_name:  { type: String, required: true, minlength: 6},
+  last_name: { type: String, required: true, maxlength: 20 },
+  age: { type: Number, min: 1, max: 150 },
+  email: { type: String, required: true }
+}, {timestamps: true });
+mongoose.model('User', UserSchema); // We are setting this Schema in our Models as 'User'
+var User = mongoose.model('User') // We are retrieving this Schema from our Models, named 'User'
+
 // Use native promises (only necessary with mongoose versions <= 4)
 mongoose.Promise = global.Promise;
 
@@ -35,30 +42,44 @@ app.set('views', path.join(__dirname, './views'));
 // Setting our View Engine set to EJS
 app.set('view engine', 'ejs');
 
+app.use(session({
+  secret: 'keyboardkitteh',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 }
+}))
+
+app.use(flash());
 
 
 // Routes
 // Root Request
 app.get('/', function(req, res) {
+  User.find({}, function(err, users) {
+    if(err){res.render('index', {error: err});} 
+    else {res.render('index', {users: users});}
     // This is where we will retrieve the users from the database and include them in the view page we will be rendering.
-    res.render('index');
-})
+  })
+});
 // Add User Request 
-app.post('/users', function(req, res) {
-    console.log("POST DATA", req.body);
-    // create a new User with the name and age corresponding to those from req.body
-    var user = new User({name: req.body.name, age: req.body.age});
-    // Try to save that new user to the database (this is the method that actually inserts into the db) and run a callback function with an error (if any) from the operation.copy
-    user.save(function(err) {
-      // if there is an error console.log that something went wrong!
-      if(err) {
-        console.log('something went wrong');
-      } else { // else console.log that we did well and then redirect to the root route
-        console.log('successfully added a user!');
+app.post('/users', function (req, res){
+  var user = new User(req.body);
+  user.save(function(err){
+      if(err){
+          // if there is an error upon saving, use console.log to see what is in the err object 
+          console.log("We have an error!", err);
+          // adjust the code below as needed to create a flash message with the tag and content you would like
+          for(var key in err.errors){
+              req.flash('registration', err.errors[key].message);
+          }
+          // redirect the user to an appropriate route
+          res.redirect('/');
       }
-      res.redirect('/');
-    })
-})
+      else {
+          res.redirect('/users');
+      }
+  });
+});
 // Setting our Server to Listen on Port: 8000
 app.listen(8000, function() {
     console.log("listening on port 8000");
