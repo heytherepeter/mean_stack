875ff2b7fcee3adb3a1661d3a8d61ff0aee7cf53
Peter ONeill <jaretep@gmail.com>
2019-05-15 11:09:44 -0700

    worked on mongoose dashboard

--- Mongoose/mongoose_dashboard/server.js
+++ Mongoose/mongoose_dashboard/server.js
@@ -9,9 +9,9 @@ const mongoose = require("mongoose");
 mongoose.connect('mongodb://localhost/mongoose_dashboard');
 
 var OwlSchema = new mongoose.Schema({
-    name: String,
-    age: Number
-})
+    name: { type: String, required: [true, 'name required'], minlength: 2},
+    age: { type: Number, required: [true, 'age required'], min: 1, max: 150 }
+}, {timestamps: true });
 mongoose.model('Owl', OwlSchema);
 var Owl = mongoose.model('Owl');
 
@@ -36,7 +36,69 @@ app.use(session({
 
 // Routing section
 app.get('/', (req, res) => {
-    res.render('index');
+    Owl.find({}, (err, owls) => {
+        if(err){
+            console.log(err);
+            res.render('index', {owls: owls, errors: err});
+        } else {
+            res.render('index', {owls: owls});
+        }
+    })
+})
+app.get('/owls/:id', (req, res) => {
+    console.log(req.params.id);
+    Owl.findOne({_id: req.params.id}, (err, owl) => {
+        if(err){
+            console.log(err);
+        }
+        console.log(owl);
+        res.render('owlInfo', {owl: owl, errors: err});
+    })
+})
+
+app.get('/owls/:id/edit', (req, res) => {
+    console.log(req.params.id);
+    Owl.findOne({_id: req.params.id}, (err, owl) => {
+        if(err){
+            console.log(err);
+        }
+        console.log(owl);
+        res.render('owlInfoEdit', {owl: owl, errors: err});
+    })
+})
+
+app.post('/owls/:id/update', (req, res) => {
+    console.log(req.body);
+    var thisOwl = Owl.findOne({_id: req.params.id});
+    thisOwl.update
+    Owl.findOne({_id: req.params.id}, (err, owl) => {
+        if(err){
+            console.log(err);
+        }
+        console.log(owl);
+        res.render('owlInfoEdit', {owl: owl, errors: err});
+    })
+})
+
+app.get('/owls/new', (req, res) => {
+    res.render('newOwl');
+})
+
+app.post('/owls/create', (req, res) => {
+    console.log(req.body);
+    var owl = new Owl({name: req.body.formName, age: req.body.formAge});
+    owl.save(function(err){
+        if(err){
+            console.log(err);
+            for(var key in err.errors){
+                req.flash('registration', err.errors[key].message);
+            };
+            res.redirect('/owls/new');
+        }
+        else {
+            res.redirect('/');
+        }
+    })
 })
 // End routing
 
