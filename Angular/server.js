ef081c78e4b2b7d9f37d820091ec4fd6a91278cb
Peter ONeill <jaretep@gmail.com>
2019-05-16 12:36:38 -0700

    finished first angular

--- Angular/server.js
+++ Angular/server.js
@@ -1,5 +1,11 @@
 const express = require('express');
 
+const port = 8000;
+
 const app = express();
 
 app.use(express.static(__dirname + '/public/dist/public'));
+
+app.listen(port, () => {
+    console.log('Listening on port: ' + port);
+});
