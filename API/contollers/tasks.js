06a22f55c489b83a2a436f3cb66a56ecc49295ab
Peter ONeill <poneill@thousandeyes.com>
2019-05-15 22:22:13 -0700

    restful api completed

---
+++ API/contollers/tasks.js
@@ -0,0 +1,59 @@
+const mongoose = require('../mongoose.js');
+const Task = mongoose.model('Task');
+
+module.exports = {
+    findAll: function(req, res) {
+        Task.find({}, (err, tasks) => {
+            let payload = {message: "This works", data: tasks};
+            if(err){
+                console.log(err);
+                payload[errors] = err;
+            };
+            res.json(payload);
+        })
+    },
+    findOne: function(req, res) {
+        Task.findOne({_id: req.params.id}, (err, task) => {
+            let payload = {message: "This works", data: task};
+            if(err){
+                console.log(err);
+                payload[errors] = err;
+            };
+            res.json(payload);
+        })
+    },
+    create: function(req, res) {
+        let newTask = new Task({
+            title: req.body.title,
+            description: req.body.description
+           });
+        newTask.save( (err) => {
+            if(err){
+                console.log(err);
+                res.json(err);
+            } else {
+                res.json(newTask);
+            }
+        } );
+    },
+    update: function(req, res) {
+        Task.update({_id: req.params.id}, req.body, (err, task) => {
+            if(err){
+                console.log(err);
+                res.json(err);
+            } else {
+                res.json(task);
+            }
+        })
+    },
+    delete: function(req, res) {
+        Task.deleteOne({_id: req.params.id}, (err) => {
+            if(err){
+                console.log(err);
+                res.json(err);
+            } else {
+                res.json({message: 'deletion successful'});
+            }
+        })
+    }
+}
--- API/server.js
+++ API/server.js
@@ -1,13 +1,3 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/restful_api');
-var TaskSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    completed: Boolean
-   }, ({}))
-   mongoose.model('Task', TaskSchema);
-var Task = mongoose.model('Task') 
-mongoose.Promise = global.Promise;
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -15,15 +5,24 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
+const Task = require('./contollers/tasks.js');
 app.get('/', (req, res) => {
-    Task.find({}, (err, tasks) => {
-        let payload = {message: "Success", data: tasks};
-        if(err){
-            console.log(err);
-            payload[errors] = err;
-        };
-        res.json(payload);
-    })
+    Task.findAll(req, res);
+})
+app.get('/tasks', (req, res) => {
+    Task.findAll(req, res);
+})
+app.post('/tasks', (req, res) => {
+    Task.create(req, res);
+})
+app.get('/tasks/:id', (req, res) => {
+    Task.findOne(req, res);
+})
+app.put('/tasks/:id', (req, res) => {
+    Task.update(req, res);
+})
+app.delete('/tasks/:id', (req, res) => {
+    Task.delete(req, res);
 })
 
 app.listen(8000, function() {
